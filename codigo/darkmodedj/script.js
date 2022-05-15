f9963e4141f6fd12e52780db896142194e7c9209
DaviCesar <81175076+DaviCesar@users.noreply.github.com>
2022-05-15 14:16:34 -0300

    Add files via upload

---
+++ codigo/darkmodedj/script.js
@@ -0,0 +1,7 @@
+const chk = document.getElementById('chk')
+const cabecalho = document.querySelector('#full_header > nav')
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+    
+})
+
