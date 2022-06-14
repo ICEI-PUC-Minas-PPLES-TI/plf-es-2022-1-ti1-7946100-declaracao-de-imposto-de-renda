82fa36dd5d62a310346dfb25ba761f3d2c65e0ec
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2022-06-14 13:02:44 -0300

    Update script.js

--- codigo/sucesso/scripts/script.js
+++ codigo/sucesso/scripts/script.js
@@ -17,3 +17,23 @@ window.onclick = function (event) {
         popup.style.display = "none";
     }
 }
+
+function alloff() {
+    var off = "off"
+    localStorage.setItem("1", "off");
+    localStorage.setItem("2", "off");
+    localStorage.setItem("3", "off");
+    localStorage.setItem("4", "off");
+}
+function salvar() {alloff()
+    localStorage.setItem("1", document.querySelector("#flexRadioDefault1").value);
+}
+function salvar2() {alloff()
+    localStorage.setItem("2", document.querySelector("#flexRadioDefault2").value);
+}
+function salvar3() {alloff()
+    localStorage.setItem("3", document.querySelector("#flexRadioDefault3").value);
+}
+function salvar4() {alloff()
+    localStorage.setItem("4", document.querySelector("#flexRadioDefault4").value);
+}
